ffd52c4f4c2d00d7639c5ce044227bcc530cf825
Fernando Olmos Lovera <fol@nowaii.com>
2022-09-13 17:27:13 -0500

    errors build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/login-metamask/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
